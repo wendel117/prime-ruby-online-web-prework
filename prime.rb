5d1c7ac47ac51dd303f448f415aca2140017d4d2
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-11 16:58:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(number)
     return false if number < 2
-    number.each do |x|
+    (2..number).each do |x|
         if (number % x) == 0
             return false
         end
