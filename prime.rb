57ce812ae04390ae8a73aa599983ae15f6055aa0
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-11 16:59:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+    return false if number < 2
+    (2..number-1).each do |x|
+        if (number % x) == 0
+            return false
+        end
+    end
+    true
+end
